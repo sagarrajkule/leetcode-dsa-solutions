a5b31796fd07a968de7a7e5f3de0fcd9669787d2
sagar rajkule <sagarrajkule22@gmail.com>
2025-07-12 11:31:39 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.42%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if (n< 2) {
-            return n;
+        if (n == 0) return 0; // Base case: F(0)
+        if (n == 1) return 1; // Base case: F(1)
+
+        // DP array to store computed Fibonacci values
+        int[] dp = new int[n + 1];
+        dp[0] = 0; // F(0)
+        dp[1] = 1; // F(1)
+
+        // Bottom-up calculation
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; // F(i) = F(i-1) + F(i-2)
         }
 
-        return fib(n-1) + fib(n-2);
+        return dp[n];
     }
 }
