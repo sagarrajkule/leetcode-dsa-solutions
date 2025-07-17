64d8d4f7ce8667aa89622221f901ca7d89d4930c
sagar rajkule <sagarrajkule22@gmail.com>
2025-07-17 21:32:38 +0530

    Time: 3 ms (70.76%), Space: 48 MB (22.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,22 +5,23 @@ class Solution {
 
         int[] dp = new int[n];
 
-        // Initialize the first row
+        // Fill the first row
         dp[0] = grid[0][0];
         for (int j = 1; j < n; j++) {
             dp[j] = dp[j - 1] + grid[0][j];
         }
 
-        // Process remaining rows
+        // Fill rest of the rows
         for (int i = 1; i < m; i++) {
-            // Update first column (can only come from top)
-            dp[0] += grid[i][0];
+            // Update first column separately (can only come from top)
+            dp[0] = dp[0] + grid[i][0];
 
             for (int j = 1; j < n; j++) {
+                // Minimum of coming from top (dp[j]) or left (dp[j - 1])
                 dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
             }
         }
 
-        return dp[n - 1]; // Result is stored in last column of last row
+        return dp[n - 1]; // Final cell (bottom-right)
     }
 }
