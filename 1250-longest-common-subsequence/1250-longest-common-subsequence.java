7d686bfa07e003768b7a0f1f931c8a9f09d73e9f
sagar rajkule <sagarrajkule22@gmail.com>
2025-07-19 12:24:42 +0530

    Time: 19 ms (87.04%), Space: 51 MB (42.3%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -3,19 +3,24 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
 
+        // dp[i][j] will store the length of the LCS of text1[0..i-1] and text2[0..j-1]
         int[][] dp = new int[n + 1][m + 1];
 
-        // Build the dp table
+        // Fill the dp table
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
+
+                // If current characters match, include it and add 1 to previous diagonal cell
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1]; // Match case
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Skip one character
+                    // Otherwise, take the max of excluding current char from either string
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
+        // The value at dp[n][m] contains the length of the longest common subsequence
         return dp[n][m];
     }
 }
