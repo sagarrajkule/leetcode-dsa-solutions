d609a22b18be1ec11ca7c82d189ac324cc0f100c
sagar rajkule <sagarrajkule22@gmail.com>
2025-07-30 22:32:57 +0530

    Time: 5 ms (56.68%), Space: 43.2 MB (75.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,22 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        // If lengths are different, they can't be anagrams
+        // Early return if lengths don't match
         if (s.length() != t.length()) return false;
 
-        Map<Character, Integer> charCount = new HashMap<>();
+        // Frequency array for characters a-z (26 letters)
+        int[] freq = new int[26];
 
-        // Count frequency of each character in string s
-        for (char c : s.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        // Increment counts from first string, decrement from second
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        // Subtract frequency using string t
-        for (char c : t.toCharArray()) {
-            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
-                return false; // Character mismatch
-            }
-            charCount.put(c, charCount.get(c) - 1);
+        // If any frequency is not zero, it's not an anagram
+        for (int count : freq) {
+            if (count != 0) return false;
         }
 
-        // All character counts matched
         return true;
     }
 }
