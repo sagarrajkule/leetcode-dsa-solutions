5b4aa15d924fd986c8dca7c0b2798a0e2e2bf78d
sagar rajkule <sagarrajkule22@gmail.com>
2025-08-03 11:54:19 +0530

    Time: 6 ms (99.52%), Space: 44.9 MB (78.13%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,38 +1,50 @@
-class Solution {
-    private int m, n;
-    private int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
+public class Solution {
+    // Main function to find the longest increasing path in the matrix
     public int longestIncreasingPath(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
-        int maxLen = 0;
-        int[][] memo = new int[m][n];  // Cache for memoization
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                maxLen = Math.max(maxLen, dfs(matrix, i, j, memo));
+        int r = matrix.length;      // Number of rows
+        int c = matrix[0].length;   // Number of columns
+        int[][] res = new int[r][c]; // Memoization array to store the longest path from each cell
+        int ans = 0;                // Variable to store the overall maximum path length
+
+        // Try DFS from every cell in the matrix
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                // Compute the longest increasing path from (i, j)
+                ans = Math.max(ans, DFS(matrix, res, i, j, r, c));
             }
         }
-        return maxLen;
+        return ans; // Return the maximum path length found
     }
-    
-    private int dfs(int[][] matrix, int i, int j, int[][] memo) {
-        // If already computed longest path from this cell, return that
-        if (memo[i][j] != 0) return memo[i][j];
-        
-        int max = 1;  // The cell itself counts as length 1
-        
-        // Explore 4 directions
-        for (int[] dir : dirs) {
-            int x = i + dir[0], y = j + dir[1];
-            // Check boundaries and increasing condition
-            if (x >= 0 && x < m && y >= 0 && y < n && matrix[x][y] > matrix[i][j]) {
-                int len = 1 + dfs(matrix, x, y, memo);
-                max = Math.max(max, len);
-            }
+
+    // Helper function: DFS with memoization
+    // Returns the longest increasing path starting from cell (i, j)
+    private int DFS(int[][] matrix, int[][] res, int i, int j, int r, int c) {
+        // If we've already found the answer for this cell, return it
+        if (res[i][j] != 0) return res[i][j];
+
+        int val = matrix[i][j]; // Current cell value
+        int maxLen = 1;         // At minimum, the path includes this cell
+
+        // Explore all four directions: up, down, left, right
+
+        // Up
+        if (i > 0 && matrix[i-1][j] > val) {
+            maxLen = Math.max(maxLen, 1 + DFS(matrix, res, i-1, j, r, c));
         }
-        
-        memo[i][j] = max;  // Memoize and return
-        return max;
+        // Down
+        if (i < r-1 && matrix[i+1][j] > val) {
+            maxLen = Math.max(maxLen, 1 + DFS(matrix, res, i+1, j, r, c));
+        }
+        // Left
+        if (j > 0 && matrix[i][j-1] > val) {
+            maxLen = Math.max(maxLen, 1 + DFS(matrix, res, i, j-1, r, c));
+        }
+        // Right
+        if (j < c-1 && matrix[i][j+1] > val) {
+            maxLen = Math.max(maxLen, 1 + DFS(matrix, res, i, j+1, r, c));
+        }
+
+        res[i][j] = maxLen; // Memoize the result for this cell
+        return maxLen;      // Return the computed longest path from (i, j)
     }
 }
